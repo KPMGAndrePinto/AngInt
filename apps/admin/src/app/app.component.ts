e129750e8c2a4b895ded9b12cd392b58f1d38552
André Pinto <ampinto@kpmg.com>
2023-03-24 14:50:47 +0000

    Admin passes tests

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'myorg-root',
   template: `
     <h1 title="Welcome admin">Welcome admin<\h1>
-    <myorg-banner title="Welcome admin"> </myorg-banner>
+    <myorg-banner title="Welcome to admin main page"> </myorg-banner>
   `,
 })
 export class AppComponent {
